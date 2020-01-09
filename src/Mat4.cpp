6fd8d35f06fd8f1c9f4905516df07965d61aa1e6
florianvazelle <ponythugflorian@gmail.com>
2020-01-09 12:06:35 +0100

    Update Mat4 class

---
+++ src/Mat4.cpp
@@ -0,0 +1,61 @@
+#include "Mat4.h"
+
+Mat4::Mat4() {
+    for (int i = 0; i < 16; i++) {
+        if (i % 5 == 0) {
+            this->data[i] = 1.0f;
+        }
+        else {
+            this->data[i] = 0.0f;
+        }
+    }
+}
+
+Mat4::Mat4(const Mat4& m) {
+    for (int i = 0; i < 16; i++) {
+        data[i] = m.data[i];
+    }
+}
+
+Mat4 Mat4::operator*(const Mat4& m) {
+    Mat4 tmp(*this);
+    return tmp *= m;
+}
+
+Mat4 Mat4::operator*=(const Mat4& m) {
+    Mat4 tmp(*this);
+    Mat4 res = Mat4();
+    int sum;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            sum = 0;
+            for (int k = 0; k < 4; k++) {
+                sum = sum + tmp.data[(i * 4) + k] * m.data[(k * 4) + j];
+            }
+            res.data[(i * 4) + j] = sum;
+        }
+    }
+    return res;
+}
+
+void Mat4::scale(float sx, float sy, float sz) {
+    this->data[0] = sx;
+    this->data[5] = sy;
+    this->data[10] = sz;
+}
+
+void Mat4::translate(float x, float y, float z) {
+    this->data[12] = x;
+    this->data[13] = y;
+    this->data[14] = z;
+}
+
+std::ostream& operator<<(std::ostream& os, const Mat4& m) {
+    for (int i = 0; i <= 16; i++) {
+        if (i % 4 == 0) {
+            std::cout << "\n";
+        }
+        std::cout << m.data[i] << ' ';
+    }
+    return os;
+}
