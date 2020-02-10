c7aba57cf3e9587526b899260b3bb10e46766ea6
florianvazelle <ponythugflorian@gmail.com>
2020-02-10 18:49:40 +0100

    Begin remplissage

--- include/Remplissage.h
+++ include/Remplissage.h
@@ -1,7 +1,9 @@
 #ifndef Remplissage_H
 #define Remplissage_H
 
+#include <vector>
+#include <nanogui/nanogui.h>
 
-//void RemplissageLCA(Poly, CR);
+void RemplissageLCA(const std::vector<Eigen::Vector2f>& Poly, const std::vector<Eigen::Vector2f>& CR);
 
 #endif
--- src/Remplissage.cpp
+++ src/Remplissage.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include "Remplissage.h"
 
-/*void RemplissageLCA(Poly, CR)
-{
-	SI = CreatSI(Poly);
-	LCA = InitLCA();
+struct Cote {
+	float ymax;
+	float xcurrent;
+	float invcoef;
+};
 
-	for ()
-		;
-}*/
+struct Maillon {
+	float ymax;
+	float xcurrent;
+	float invcoef;
+};
+
+
+void CreatSI(std::vector<Maillon>& SI, const std::vector<Eigen::Vector2f>& Poly) {
+
+}
+
+void InitLCA(std::vector<Cote>& LCA) {
+
+}
+
+void RemplissageLCA(const std::vector<Eigen::Vector2f>& Poly, const std::vector<Eigen::Vector2f>& CR)
+{
+	std::vector<Maillon> SI;
+	CreatSI(SI, Poly);
+
+	  
+	std::vector<Cote> LCA;
+	InitLCA(LCA);
+}
